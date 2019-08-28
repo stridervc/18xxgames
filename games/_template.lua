a7642cf9fd80f91647526f60df18f65f62a83a48
Fred Strauss <fred@strider.co.za>
2019-08-28 14:42:30 +0200

    Add 1889

---
+++ games/_template.lua
@@ -0,0 +1,12 @@
+-- game template
+-- http://18xx.info/{filename}
+
+Game {
+	title		= "18Template",
+	description	= "Long form description of the game",
+	players		= {2,6},
+	bggid		= 23540,
+	cap			= fullcap,
+	playtime	= 180,		-- minutes
+	url			= "http://example.com",
+}
