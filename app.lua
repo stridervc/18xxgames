1f811580791e5aea6d8147ee5561173f9027c77e
Fred Strauss <redshift@18xx.info>
2019-08-28 14:38:05 +0200

    Add layout

--- app.lua
+++ app.lua
@@ -1,10 +1,13 @@
 local lapis = require("lapis")
 local app = lapis.Application()
 
-app:enable("etlua")	-- html templates
+app:enable("etlua")	-- enable html templates
+
+-- layout template
+app.layout = require "views.layout"
 
 app:get("/", function()
-  return {render = "index"}
+  return {render = "index"}	-- views/index.etlua
 end)
 
 return app
