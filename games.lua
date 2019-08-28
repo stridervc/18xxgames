2041d7f4d3521a1ef4be7d4f7ff5b6d454c39c3e
Fred Strauss <redshift@18xx.info>
2019-08-28 17:40:16 +0200

    More work on games

--- games.lua
+++ games.lua
@@ -6,7 +6,7 @@ local terms = require("terms")
 -- template
 Game {
 	--title		= "18Template",	-- this has to be set
-	description	= "Long form description of the game",
+	subtitle	= "Long form description of the game",
 	players		= {2,6},
 	bggid		= 23540,
 	cap			= fullcap,
@@ -17,21 +17,46 @@ Game {
 -- 1830
 Game {
 	title		= "1830",
-	description	= "Railways & Robber Barons",
-	players		= {2,7},
+	subtitle	= "Railways & Robber Barons",
+	basics		= {
+		["Number of players"]	= "2 to 7",
+		["Estimated play time"]	= "360 minutes",
+		Capitalisation			= terms["fullcap"],
+	},
 	bggid		= 421,
-	cap			= terms["fullcap"],
-	playtime	= 360,		-- minutes
+}
+
+-- 1846
+Game {
+	title		= "1846",
+	subtitle	= "The Race for the Midwest",
+	basics		= {
+		["Number of players"]	= "3 to 5 (2 with rules on BGG)",
+		["Estimated play time"]	= "240 minutes",
+		Capitalisation			= terms["partialcap"],
+	},
+	bggid		= 17405,
+	section		= {
+		Setup	= {
+			"Draw for seating and priority deal",
+			"<table><tr><th>Number of players</th><th>3</th><th>4</th><th>5</th></tr>",
+			"<tr><th>Initial Bank Size</th><td>6 500</td><td>7 500</td><td>9 000</td></tr>",
+			"</table>",
+			"Each player starts with $400 from this bank.",
+		},
+	},
 }
 
 -- 1889
 Game {
 	title		= "1889",
-	description	= "History of Shikoku railways",
-	players		= {2,6},
+	subtitle	= "History of Shikoku railways",
+	basics		= {
+		["Number of players"]	= "2 to 6",
+		["Estimated play time"]	= "210 minutes",
+		Capitalisation			= terms["fullcap"],
+	},
 	bggid		= 23540,
-	cap			= terms["fullcap"],
-	playtime	= 210,		-- minutes
 	section		= {
 		Setup	= "Detailed setup instructions will be added here.",
 	},
