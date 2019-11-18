a22396d0a8b72347f857536948ba9921f708c5a0
Fred Strauss <fred@strider.co.za>
2019-11-18 21:32:37 +0200

    Start adding 1861 PnP info

---
+++ games/1861.lua
@@ -0,0 +1,8 @@
+Game {
+	title			= "1861",
+	bggid			= 10391,
+
+	pnpurl			= "https://sites.google.com/a/all-aboardgames.com/aagllc/home/1861-the-railways-of-the-russian-empire"
+	pnplicense		= "Creative Commons Attribution-NonCommercial",
+	pnplicenseurl	= "https://creativecommons.org/licenses/by-nc/4.0/",
+}
